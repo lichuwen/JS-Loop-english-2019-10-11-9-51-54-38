afb1133983a4d3e98968e18765be6c40a830e7ab
lichuwen <37268385+lichuwen@users.noreply.github.com>
2020-06-19 16:02:39 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,11 @@
+function number(){
+  	for(var i=1; i<=20; i++){
+  		if(i % 2 == 0){
+  			document.write(i + " is even number." + '<br>')
+  		}
+  		else{
+			document.write(i + " is odd number." + '<br>')
+  		}
+  	}
+}
+number();
